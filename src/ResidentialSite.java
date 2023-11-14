fdae8a6d5a3d0fcdfe76a5684fc78546804c2d60
Caleb Lui-Yee <113129278+Waluiyeegi@users.noreply.github.com>
2023-11-14 18:08:49 -0500

    deleted getbilleable amount

--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -1,7 +1,16 @@
 public class ResidentialSite implements Site{
     public double getBillableAmount(double units, double rate){
-        double base = units * rate;
-        double tax = base * Site.taxRate;
+        double base = getBase(units, rate);
+        double tax = getTax(base);
         return base + tax;
+    }
+
+    private double getTax(double base) {
+        double tax = base * Site.taxRate;
+        return tax;
+    }
+
+    private double getBase(double units, double rate) {
+        return units * rate;
     }
 }
