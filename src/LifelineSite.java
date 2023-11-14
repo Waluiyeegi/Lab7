546b4f95ea8456101d857123084f7fbd4459bddf
Caleb Lui-Yee <113129278+Waluiyeegi@users.noreply.github.com>
2023-11-14 17:43:43 -0500

    added get base

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,7 +1,11 @@
 public class LifelineSite implements Site{
     public double getBillableAmount(double units, double rate){
-        double base = units * rate * 0.5;
+        double base = getBase(units, rate);
         double tax = base * Site.taxRate * 0.2;
         return base + tax;
+    }
+
+    private double getBase(double units, double rate) {
+        return units * rate * 0.5;
     }
 }
--- src/Site.java
+++ src/Site.java
@@ -1,3 +1,4 @@
 public interface Site {
     double taxRate = 1.15;
+    double getBillableAmount(double units, double rate);
 }
