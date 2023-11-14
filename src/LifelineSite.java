eac66557227e03fc9b2abd4a94d833905ccc8535
Caleb Lui-Yee <113129278+Waluiyeegi@users.noreply.github.com>
2023-11-14 18:04:04 -0500

    deleted getbilleable amount

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,9 +1,4 @@
 public class LifelineSite implements Site{
-    public double getBillableAmount(double units, double rate){
-        double base = getBase(units, rate);
-        double tax = getTax(base);
-        return base + tax;
-    }
 
     private double getTax(double base) {
         return base * Site.taxRate * 0.2;
